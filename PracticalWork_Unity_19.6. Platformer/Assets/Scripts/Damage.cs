be51339987c1ee78d8969b580086db9ac0af0714
finashin82 <finashin82@mail.ru>
2024-06-12 21:49:13 +0900

    Общий Damage
    Общий Damage вместо DamageDealler и DamageToThePlayer.
    Только уровень 3.

--- PracticalWork_Unity_19.6. Platformer/Assets/Scripts/Damage.cs
+++ PracticalWork_Unity_19.6. Platformer/Assets/Scripts/Damage.cs
@@ -2,14 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageDealler : MonoBehaviour
+public class Damage : MonoBehaviour
 {
     // Урон от пули
     [SerializeField] private float damage;
 
-    // Время, через которое уничтожается пуля
-    private int timeToDestroy = 1;
-   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Если у объекта есть скрипт с жизнью, то можем нанести ему урон
@@ -17,18 +14,6 @@ public class DamageDealler : MonoBehaviour
         {
             // У объекта с которым столкнулись пули, вызывается скрипт "Health" и метод "TakeDamage" с параметром damage (Уменьшает здоровье)
             collision.gameObject.GetComponent<Health>().TakeDamage(damage);
-        }               
-
-        //Coroutine coroutin = StartCoroutine(timer());
-    }
-
-    /// <summary>
-    /// Вызываем уничтожение пули через определенное время
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator timer()
-    {        
-        yield return new WaitForSeconds(timeToDestroy);
-        Destroy(gameObject);        
+        }
     }
 }
